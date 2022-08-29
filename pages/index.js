10bcdc5d5fccb7741d2d48e991b1edbce579f482
Matt <98845506+mutthias@users.noreply.github.com>
2022-08-28 17:17:11 -0700

    css fix and basic setup

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Link from 'next/link';
-import Image from 'next/Image';
+import Image from 'next/image';
 import {Flex, Box, Text, Button} from '@chakra-ui/react';
 
-const Banner = ( {purpose} ) => (
-  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-    <Image src={imageURL} width
+const Banner = ( {purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl} ) => (
+  <Flex flexWrap='wrap' justifyContent='center' alignItems='center' margin='10' color='red.900'>
+    <Image src={imageUrl} width={500} height = {300} alt="banner" />
+    <Box p='5'>
+      <Text color='grey.500' font-size="sm" fontWeight='medium'></Text>
+      <Text color='3xl' font-size="bold"> {title1}<br />{title2} </Text>
+      <Text font-size="lg" paddingTop='3' color="gray.700">{desc1} <br /> {desc2} </Text>
+      <Button fontSize="xl" bg='blue.300' color='white'>
+        <Link href={linkName}>{buttonText}</Link>
+      </Button>
+    </Box>
   </Flex>
 )
 
@@ -12,8 +20,28 @@ export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Banner purpose={'For Sale'} />
-      <Banner purpose={'For Rent'} />
+      <Banner 
+        purpose="Rent a Home"
+        title="Rental Homes for"
+        title2='Everyone'
+        desc1='Explore Apartments, Villas, Homes'
+        desc2='and more'
+        buttonText='Explore Renting'
+        linkName='/search?purpose=for-rent'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
+
+      />
+      <Banner 
+        purpose="Buy a Home"
+        title="Find, Buy and Own Your"
+        title2='Dream Home'
+        desc1='Explore Apartments, Villas, Homes'
+        desc2='and more'
+        buttonText='Explore Buying'
+        linkName='/search?purpose=for-sale'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
+      
+      />
 
     </div>
   )
