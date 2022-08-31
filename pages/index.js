d67508f6db1713904d92bdb40a374618fc4c023a
Matt <98845506+mutthias@users.noreply.github.com>
2022-08-30 22:27:49 -0700

    continuation of app

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,32 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import {Flex, Box, Text, Button} from '@chakra-ui/react';
+import Property from '../components/Property'
+
+import {baseUrl, fetchApi} from '../utils/fetchApi';
 
 const Banner = ( {purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl} ) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' margin='10' color='red.900'>
     <Image src={imageUrl} width={500} height = {300} alt="banner" />
     <Box p='5'>
-      <Text color='grey.500' font-size="sm" fontWeight='medium'></Text>
-      <Text color='3xl' font-size="bold"> {title1}<br />{title2} </Text>
-      <Text font-size="lg" paddingTop='3' color="gray.700">{desc1} <br /> {desc2} </Text>
-      <Button fontSize="xl" bg='blue.300' color='white'>
+      <Text color='grey.500' fontSize="sm" fontWeight='medium'>{purpose}</Text>
+      <Text color='3xl' as='b'> {title1} <br /> {title2} </Text>
+      <Text fontSize="lg" paddingTop='3' color="gray.700">{desc1} <br /> {desc2} </Text>
+      <Button fontSize="xl">
         <Link href={linkName}>{buttonText}</Link>
       </Button>
     </Box>
   </Flex>
 )
 
-export default function Home() {
+export default function Home( {propertiesForSale, propertiesForRent}) {
+
   return (
-    <div>
+    <Box>
       <h1>Hello World</h1>
       <Banner 
         purpose="Rent a Home"
-        title="Rental Homes for"
+        title1="Rental Homes for"
         title2='Everyone'
         desc1='Explore Apartments, Villas, Homes'
         desc2='and more'
@@ -31,18 +35,38 @@ export default function Home() {
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
 
       />
+
+      <Flex flexWrap="wrap">
+        {propertiesForRent.map((property) => <Property property ={property} key={property.id} />)}
+      </Flex>
+
+
       <Banner 
         purpose="Buy a Home"
-        title="Find, Buy and Own Your"
+        title1="Find, Buy and Own Your"
         title2='Dream Home'
         desc1='Explore Apartments, Villas, Homes'
         desc2='and more'
         buttonText='Explore Buying'
         linkName='/search?purpose=for-sale'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
-      
       />
-
-    </div>
+      {propertiesForSale.map((property) => <Property property ={property} key={property.id} />)}
+    </Box>
   )
 }
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+
+    }
+  }
+
+}
+
