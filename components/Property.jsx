c159ea7b235a793510d87e1ddc3a4b18bce14991
Matt <98845506+mutthias@users.noreply.github.com>
2022-09-07 19:53:58 -0700

    responsiveness and imagescrollbar

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Box, Flex, Text, Avatar } from '@chakra-ui/react';
-import {FaBed, FaBath} from 'react-icons/fa'
-import {BsGridFill} from 'react-icons/bs'
-import {GoVerified } from 'react-icons/go'
+import { FaBed, FaBath } from 'react-icons/fa'
+import { BsGridFill } from 'react-icons/bs'
+import { GoVerified } from 'react-icons/go'
 import millify from "millify";
 import DefaultImage from '../assets/images/drake.jpg';
 
